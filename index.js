19c9519a0932afaaaec33192edebce66379f9011
yobbad16 <dabboyrey@gmail.com>
2020-05-22 21:14:44 +0800

    Added a start server script, added entry point html file,created simple component and render to the page

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1> Parcel setup like create-react-app</h1>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
